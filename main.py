59121a3d1cb9b72ae7cc4f266133370c65c36e02
Grady Blackwell <hattielover0131@gmail.com>
2026-03-24 15:25:44 -0600

    initial commit I suppose

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, session, send_from_directory, request, jsonify, redirect, url_for #type: ignore
+from flask_cors import CORS #type: ignore
+from supabase import create_client, Client #type: ignore
+import os
+from dotenv import load_dotenv #type: ignore
+from postgrest.exceptions import APIError #type: ignore
+load_dotenv()
+secret_key = os.environ.get("SECRET_KEY")
+supabase_key = os.environ.get("SUPABASE_KEY")
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase = None
+app = Flask(__name__, 
+    static_folder=os.path.join(os.path.dirname(__file__), "static"),
+    static_url_path="/static")
+app.config['SECRET_KEY'] = secret_key
+appUrl = "https://127.0.0.1:8000"
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": appUrl}})
+def get_supabase():
+    global supabase
+    if supabase is None:
+        supabase = create_client(supabase_url, supabase_key)
+    return supabase
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": appUrl}})
+@app.route("/", methods=['GET'])
+def serve_index():
+    return render_template("index.html", supabase_url=supabase_url, supabase_anon_key = os.environ.get("SUPABASE_ANON_KEY"))
+@app.route("/api/submitmessage", methods=['POST'])
+def submit_message():
+    data = request.json
+    if "content" not in data:
+        return jsonify({'error':'empty message'}), 400
+    elif len(data['content']) > 5000:
+        return jsonify({'error':'message too long'}), 400
+    if data['sender'].strip() == "" or data['content'].strip() == "":
+        return jsonify({'error':'one or more fields is empty. stop editing the JS pls.'})
+    try:
+        response = get_supabase().table("messages").insert(data).execute()
+        return jsonify(response.data), 200
+    except APIError as e:
+        return jsonify({'error':e})
+@app.route("/api/getmessages", methods=['POST'])
+def get_messages():
+    data = request.json
+    if (data['room'] == None):
+        return jsonify({'error':'no room found'}), 
+    else:
+        try:
+            response = get_supabase().table('messages').select('sender', 'content').execute()
+            return jsonify(response.data), 200
+        except APIError as e:
+            return jsonify({'error':e}), 400
+
+
+    
+    
---
+++ static/js/chat.js
@@ -0,0 +1,86 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+const appUrl = window.location.origin;
+const messagesDiv = document.getElementById("messagesDiv")
+supabase
+    .channel("db-changes")
+    .on("postgres_changes", { event: "*", schema: "public", table: "messages" }, (payload) => {
+        process_change(payload)
+    })
+    .subscribe();
+async function submitMessage() {
+    var data = {'sender': document.getElementById('nameInput').innerText, 'content': document.getElementById("msgInput").value }
+    if (data['content'].trim() == "") {
+        return
+    }
+    const res = await fetch(appUrl + '/api/submitmessage', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    if (!res.ok) {
+        response = await res.json()
+        console.log(response['e'])
+    } else {
+        document.getElementById("msgInput").value = "";
+    }
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+async function getMessages() {
+    console.log("Fetching messages...")
+    var data = {};
+    data['room'] = 'general'
+    const res = await fetch(appUrl + '/api/getmessages', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    if (res.ok) {
+        var response = await res.json()
+        for (var i in response) {
+            var message = response[i]
+            if (message['content'] !== undefined && message['sender'] !== undefined) {
+                var messageP = document.createElement("p");
+                messageP.innerHTML = `[<b>${message['sender']}</b>] ${message['content']}`;
+                messagesDiv.appendChild(messageP);
+            }
+        }
+    }
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        submitMessage()
+    }
+})
+document.addEventListener("keyup", (e)=>{
+    if (document.getElementById("nameInput").innerHTML == "") {
+        document.getElementById("nameInput").innerHTML = " "
+    }
+})
+
+document.getElementById('nameInput').addEventListener('keypress', (evt) => {
+    if (evt.key == "Enter") {
+        evt.preventDefault();
+        return
+    }
+    if (document.getElementById("nameInput").innerText.length >= 10) {
+        evt.preventDefault();
+    }
+});
+
+async function process_change(payload) {
+    console.log(payload)
+    if (payload['eventType'] == "INSERT") {
+        console.log(payload['new']['sender'] + " sent a message saying " + payload['new']['content'])
+        var messageP = document.createElement("p");
+        messageP.innerHTML = `<b>[${payload['new']['sender']}]</b> ${payload['new']['content']}`;
+        messagesDiv.appendChild(messageP);
+    }
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+getMessages()
